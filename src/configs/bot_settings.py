44a126662bf793b8b36cde234452e4359ce0355f
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2023-10-18 23:25:00 +0300

    Update: add day sorting pairs

--- src/configs/bot_settings.py
+++ src/configs/bot_settings.py
@@ -1,7 +1,8 @@
+import datetime
 from os import getenv
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from typing import List, Optional
+from typing import List
 
 load_dotenv()
 
@@ -12,3 +13,24 @@ IDS: List[int] = [int(id) for id in (env_admins_ID or "").split(",") if id.strip
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+year = datetime.date.today().year
+
+
+group_name = 'кб-203'   
+semester = '1'
+semester_part = '1'
+
+url = f'https://student{year}.lpnu.ua/students_schedule?studygroup_abbrname={group_name}&semestr={semester}&semestrduration={semester_part}'
+
+text: list[str] = []
+
+day_pairs: dict[str, list[str]] = {
+    'Пн': [],
+    'Вт': [],
+    'Ср': [],
+    'Чт': [],
+    'Пт': [],
+    'Сб': [],
+    'Нд': []
+}
