c4a7a71a3e25483709f9c1858a0c5c3d6dc81ab8
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2023-10-19 20:08:29 +0300

    UPDATE: upgrad parcing

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,24 +1,65 @@
 import re
-from aiogram import F
+from aiogram import F, Router
 from aiogram.types import Message
 from src.parser.schedule_parcer import Schedule
-from src.configs.bot_settings import dp, bot, url, text, day_pairs
+from src.configs.bot_settings import dp, bot,  text, day_pairs, search_ids, year
 from src.configs.bot_answers import greet_text
 from src.bot.kbs import main_kb
 
-parser = Schedule(day_pairs=day_pairs, url=url, text=text)
+get_group = False
+
+url = ''
+
 
 @dp.message(F.text == '/start')
 async def get_group_info(message: Message):
     await message.answer(greet_text, reply_markup=main_kb)
-
+    await message.answer(url)
 
 @dp.message(F.text == '/schedule')
-async def parse(message: Message): 
-    await message.answer(url) 
-    parser.get_schedule() 
+async def parse(message: Message):
+    await message.answer(url)  # Print the current URL
+    parser = Schedule(day_pairs=day_pairs, url=url, search_ids=search_ids, text=text)
+
+    # Fetch the schedule using the current URL
+    parser.get_schedule(url)
+
+    formatted_schedule= ' '
+    for day, lessons in day_pairs.items():
+        if lessons:
+            formatted_schedule = f"*{day}*\n\n"
+            for lesson in lessons:
+                formatted_schedule += f"{lesson.strip()}\n\n"
+            await message.answer(formatted_schedule)
+    formatted_schedule= ' '
 
 
+
+@dp.message(F.text == '/selectGroup')
+async def parse(message: Message):
+    global get_group 
+    get_group = True
+    await message.answer('Відправте номер групи семестр та яка частина\n Наприклад(кб-203 1 1)')
+
+
+@dp.message()
+async def get_group(message: Message):
+    global url, get_group, parser
+    pattern = r"(\S+) (\d+) (\d+)"
+    input_string = message.text
+    match = re.search(pattern, input_string)
+    
+    if get_group:
+        if match:
+            group_name = match.group(1)
+            semester = match.group(2)
+            semester_part = match.group(3)
+            url = f'https://student{year}.lpnu.ua/students_schedule?studygroup_abbrname={group_name}&semestr={semester}&semestrduration={semester_part}'
+            get_group = False
+        else:
+            await message.answer("Помилка: не вдалося знайти всі значення")
+   
+        
 # main function to start polling bot
 async def bot_main():
     await dp.start_polling(bot, skip_updates=True)
